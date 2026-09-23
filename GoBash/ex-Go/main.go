d33933717f5d76ca5dd6551852628ac249134b52
takuma.shiraki <takuma.shiraki@optim.co.jp>
2026-09-23 20:04:23 +0900

    🔧 デモ用サーバーのGo/Nodeバージョンを固定 #6

--- GoBash/ex-Go/main.go
+++ GoBash/ex-Go/main.go
@@ -5,7 +5,7 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5" // サーバーを起動するパッケージ
 )
 
 func main() {
@@ -14,6 +14,8 @@ func main() {
 		fmt.Fprintln(w, "Hello, Go server!")
 	})
 
-	log.Println("server started at http://localhost:8080")
+	log.Println("Go Bash vol.3")
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
+
+// https://carbon.now.sh/?bg=rgba%28171%2C+184%2C+195%2C+1%29&t=vscode&wt=none&l=javascript&width=570&ds=true&dsyoff=0px&dsblur=0px&wc=true&wa=false&pv=0px&ph=0px&ln=false&fl=1&fm=Hack&fs=14px&lh=133%25&si=false&es=2x&wm=false&code=const%2520express%2520%253D%2520require%28%2522express%2522%29%253B%2520%252F%252F%2520%25E3%2582%25B5%25E3%2583%25BC%25E3%2583%2590%25E3%2583%25BC%25E3%2582%2592%25E8%25B5%25B7%25E5%258B%2595%25E3%2581%2599%25E3%2582%258B%25E3%2583%2591%25E3%2583%2583%25E3%2582%25B1%25E3%2583%25BC%25E3%2582%25B8%250Aconst%2520app%2520%253D%2520express%28%29%253B%250A%250Aapp.get%28%2522%252F%2522%252C%2520%28req%252C%2520res%29%2520%253D%253E%2520%257B%250A%2520%2520res.send%28%2522Hello%252C%2520Node%2520server%21%255Cn%2522%29%253B%250A%257D%29%253B%250A%250Aapp.listen%288080%252C%2520%28%29%2520%253D%253E%2520%257B%250A%2520%2520console.log%28%2522Go%2520Bash%2520vol.3%2522%29%253B%250A%257D%29%253B
--- GoBash/ex-npm/index.js
+++ GoBash/ex-npm/index.js
@@ -1,5 +1,4 @@
-// ex-Go/main.go と同じことをする実装
-const express = require("express");
+const express = require("express");  // サーバーを起動するパッケージ
 
 const app = express();
 
@@ -8,5 +7,7 @@ app.get("/", (req, res) => {
 });
 
 app.listen(8080, () => {
-  console.log("server started at http://localhost:8080");
+  console.log("Go Bash vol.3");
 });
+
+// https://carbon.now.sh/?bg=rgba%28171%2C+184%2C+195%2C+1%29&t=vscode&wt=none&l=text%2Fx-go&width=680&ds=true&dsyoff=0px&dsblur=0px&wc=true&wa=true&pv=0px&ph=0px&ln=false&fl=1&fm=Hack&fs=14px&lh=133%25&si=false&es=2x&wm=false&code=package%2520main%250A%250Aimport%2520%28%250A%2509%2522fmt%2522%250A%2509%2522log%2522%250A%2509%2522net%252Fhttp%2522%250A%250A%2509%2522github.com%252Fgo-chi%252Fchi%252Fv5%2522%2520%252F%252F%2520%25E3%2582%25B5%25E3%2583%25BC%25E3%2583%2590%25E3%2583%25BC%25E3%2582%2592%25E8%25B5%25B7%25E5%258B%2595%25E3%2581%2599%25E3%2582%258B%25E3%2583%2591%25E3%2583%2583%25E3%2582%25B1%25E3%2583%25BC%25E3%2582%25B8%250A%29%250A%250Afunc%2520main%28%29%2520%257B%250A%2509r%2520%253A%253D%2520chi.NewRouter%28%29%250A%2509r.Get%28%2522%252F%2522%252C%2520func%28w%2520http.ResponseWriter%252C%2520req%2520*http.Request%29%2520%257B%250A%2509%2509fmt.Fprintln%28w%252C%2520%2522Hello%252C%2520Go%2520server%21%2522%29%250A%2509%257D%29%250A%250A%2509log.Println%28%2522Go%2520Bash%2520vol.3%2522%29%250A%2509log.Fatal%28http.ListenAndServe%28%2522%253A8080%2522%252C%2520r%29%29%250A%257D
